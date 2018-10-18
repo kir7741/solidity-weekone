190528575dba1705b82b57d4fde321d304562e8f
Joseph Lin <joseph@xtremeapp.com.tw>
2018-10-19 00:50:45 +0800

    [甜點] 完成手機版甜點頁UI

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,9 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+
+  products = [
+    { imgUrl: '../../assets/image/brenda-godinez.jpg' },
+    { imgUrl: '../../assets/image/heather-schwartz.jpg' },
+    { imgUrl: '../../assets/image/brooke-lark2.jpg' },
+    { imgUrl: '../../assets/image/food-photographer2.jpg' },
+    { imgUrl: '../../assets/image/food-cake.jpg' },
+    { imgUrl: '../../assets/image/brenda-godinez.jpg' },
+  ];
 
   constructor() { }
 
